d396099919e94fa50fa1b7378df409f8e441221a
jbr79 <jbr79@ef0d8562-5c19-0410-972e-841db63a069c>
2006-08-11 03:39:18 +0000

    move exponent functions to exponent.c & exponent.h
    git-svn-id: https://aften.svn.sourceforge.net/svnroot/aften@39 ef0d8562-5c19-0410-972e-841db63a069c

---
+++ libaften/exponent.h
@@ -0,0 +1,37 @@
+/**
+ * Aften: A/52 audio encoder
+ * Copyright (c) 2006  Justin Ruggles <jruggle@earthlink.net>
+ *
+ * Based on "The simplest AC3 encoder" from FFmpeg
+ * Copyright (c) 2000 Fabrice Bellard.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/**
+ * @file exponent.h
+ * A/52 exponent header
+ */
+
+#ifndef EXPONENT_H
+#define EXPONENT_H
+
+#include "a52.h"
+
+extern void expsizetab_init();
+
+extern void process_exponents(A52Context *ctx);
+
+#endif /* EXPONENT_H */
