6ca70525275931b78d9409cb2600bfa355dae520
jbr79 <jbr79@ef0d8562-5c19-0410-972e-841db63a069c>
2006-10-23 06:28:06 +0000

    add dynamic range compression encoding
    git-svn-id: https://aften.svn.sourceforge.net/svnroot/aften@183 ef0d8562-5c19-0410-972e-841db63a069c

---
+++ libaften/dynrng.h
@@ -0,0 +1,40 @@
+/**
+ * Aften: A/52 audio encoder
+ * Copyright (c) 2006  Justin Ruggles <jruggle@earthlink.net>
+ *
+ * Based on "The simplest AC3 encoder" from FFmpeg
+ * Copyright (c) 2000 Fabrice Bellard.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/**
+ * @file dynrng.h
+ * A/52 Dynamic Range Compression header
+ */
+
+#ifndef DYNRNG_H
+#define DYNRNG_H
+
+#include "common.h"
+
+#include "aften.h"
+
+void dynrng_init();
+
+int calculate_block_dynrng(FLOAT *samples[A52_MAX_CHANNELS], int num_ch,
+                           int dialnorm, enum DynRngProfile profile);
+
+#endif
