6887b6397b7563c8a2f4fa438991401c408a2b3d
prakashkc <prakashkc@ef0d8562-5c19-0410-972e-841db63a069c>
2007-01-27 18:19:25 +0000

    correct last change
    git-svn-id: https://aften.svn.sourceforge.net/svnroot/aften@270 ef0d8562-5c19-0410-972e-841db63a069c

--- util/wavrms.c
+++ util/wavrms.c
@@ -140,7 +140,7 @@ main(int argc, char **argv)
         nr = wavfile_read_samples(&wf, buf, frame_size);
     }
     avg_rms /= avg_cnt;
-    printf("Dialnorm: -%ul dB\n", MIN(avg_rms, 31));
+    printf("Dialnorm: -%lu dB\n", MIN(avg_rms, 31));
 
     free(buf);
     fclose(fp);
