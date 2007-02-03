e79cab0c491f9f0055d9507a39bf854c04694648
prakashkc <prakashkc@ef0d8562-5c19-0410-972e-841db63a069c>
2007-02-03 16:39:58 +0000

    fill binding; need to test though
    git-svn-id: https://aften.svn.sourceforge.net/svnroot/aften@314 ef0d8562-5c19-0410-972e-841db63a069c

--- bindings/aftenxx.cxx
+++ bindings/aftenxx.cxx
@@ -18,37 +18,70 @@
  ********************************************************************************/
 #include "aftenxx.h"
 
-//FIXME: Turn it into real binding...
-
 namespace Aften
 {
+#include "aften.h"
+
 /// Constructor: Initializes the encoder with specified context
-FrameEncoder::FrameEncoder(AftenContext &context){}
+FrameEncoder::FrameEncoder(AftenContext &context)
+{
+    m_context = context;
+    int nReturnValue = aften_encode_init(&m_context);
+    if (nReturnValue > 0)
+        throw nReturnValue;
+}
 
 /// Destructor: Deinitalizes the encoder
-FrameEncoder::~FrameEncoder(){}
+FrameEncoder::~FrameEncoder()
+{
+    aften_encode_close(&m_context);
+}
 
 /// Encodes PCM samples to an A/52 frame
-int FrameEncoder::Encode(unsigned char *frameBuffer, void *samples){}
+int FrameEncoder::Encode(unsigned char *frameBuffer, void *samples)
+{
+    return aften_encode_frame(&m_context, frameBuffer, samples);
+}
 
 /// Gets a context with default values
-AftenContext FrameEncoder::GetDefaultsContext(){}
+AftenContext FrameEncoder::GetDefaultsContext()
+{
+    AftenContext context;
+    aften_set_defaults(&context);
+    return context;
+}
 
 /// Determines the proper A/52 acmod and lfe parameters based on the
 /// WAVE_FORMAT_EXTENSIBLE channel mask.  This is more accurate than assuming
 /// that all the proper channels are present.
-void FrameEncoder::WaveChannelMaskToAcmod(int channels, int channelMask, int &acmod, bool &hasLfe){}
+void FrameEncoder::WaveChannelMaskToAcmod(int channels, int channelMask, int &acmod, bool &hasLfe)
+{
+    int nLfe;
+    aften_wav_chmask_to_acmod(channels, channelMask, &acmod, &nLfe);
+    hasLfe = nLfe != 0;
+}
 
 /// Determines probable A/52 acmod and lfe parameters based on the number of
 /// channels present.  You can use this if you don't have the channel mask.
-void FrameEncoder::PlainWaveToAcmod(int channels, int &acmod, bool &hasLfe){}
+void FrameEncoder::PlainWaveToAcmod(int channels, int &acmod, bool &hasLfe)
+{
+    int nLfe;
+    aften_plain_wav_to_acmod(channels, &acmod, &nLfe);
+    hasLfe = nLfe != 0;
+}
 
 /// Takes a channel-interleaved array of audio samples, where the channel order
 /// is the default Wave order. The samples are rearranged to the proper A/52
 /// channel order based on the acmod and lfe parameters.
 void FrameEncoder::RemapWaveToA52(void *samples, int samplesCount, int channels,
-                           enum A52SampleFormat format, int acmod){}
+                           enum A52SampleFormat format, int acmod)
+{
+    aften_remap_wav_to_a52(samples, samplesCount, channels, format, acmod);
+}
 
 /// Tells whether libaften was configured to use floats or doubles
-enum FloatType FrameEncoder::GetFloatType(){}
+enum FloatType FrameEncoder::GetFloatType()
+{
+    return aften_get_float_type();
+}
 }
