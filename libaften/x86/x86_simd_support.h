ca827a54bf942932c77d2c92cb29be97cc286ead
prakashkc <prakashkc@ef0d8562-5c19-0410-972e-841db63a069c>
2007-03-29 14:49:41 +0000

    fix simd support
    git-svn-id: https://aften.svn.sourceforge.net/svnroot/aften@480 ef0d8562-5c19-0410-972e-841db63a069c

--- libaften/x86/x86_simd_support.h
+++ libaften/x86/x86_simd_support.h
@@ -21,31 +21,16 @@
 
 #include "mem.h"
 
-#include <xmmintrin.h>
-
-#ifdef USE_SSE3
-#include <pmmintrin.h>
-
-#ifdef EMU_CASTSI128
-#define _mm_castsi128_ps(X) ((__m128)(X))
-#endif
-
-#define _mm_lddqu_ps(x) _mm_castsi128_ps(_mm_lddqu_si128((__m128i*)(x)))
-#else
-
-#define _mm_lddqu_ps(x) _mm_loadu_ps(x)
-#endif /* USE_SSE3 */
-
-#ifndef _MM_ALIGN16
-#define _MM_ALIGN16  __attribute__((aligned(16)))
-#endif
-
-#define PM128(x) (*(__m128*)(x))
+#ifdef USE_MMX
+#include <mmintrin.h>
 
 union __m64ui {
     unsigned int ui[4];
     __m64 v;
 };
+
+#ifdef USE_SSE
+#include <xmmintrin.h>
 
 union __m128ui {
     unsigned int ui[4];
@@ -57,9 +42,33 @@ union __m128f {
     __m128 v;
 };
 
+#define _mm_lddqu_ps(x) _mm_loadu_ps(x)
+
+#ifdef USE_SSE2
+#include <emmintrin.h>
+
 union __m128iui {
     unsigned int ui[4];
     __m128i v;
 };
 
+#ifdef USE_SSE3
+#include <pmmintrin.h>
+
+#ifdef EMU_CASTSI128
+#define _mm_castsi128_ps(X) ((__m128)(X))
+#endif /* EMU_CASTSI128 */
+
+#undef _mm_lddqu_ps
+#define _mm_lddqu_ps(x) _mm_castsi128_ps(_mm_lddqu_si128((__m128i*)(x)))
+#endif /* USE_SSE3 */
+#endif /* USE_SSE2 */
+
+#ifndef _MM_ALIGN16
+#define _MM_ALIGN16  __attribute__((aligned(16)))
+#endif
+
+#define PM128(x) (*(__m128*)(x))
+#endif /* USE_SSE */
+#endif /* USE_MMX */
 #endif /* X86_SIMD_SUPPORT_H */
