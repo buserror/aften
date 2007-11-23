b2dc2c3b5c43cf8c8f77877da51690877206843e
prakashkc <prakashkc@ef0d8562-5c19-0410-972e-841db63a069c>
2007-11-23 15:39:53 +0000

    more convenience overloads for Encoding and some cosmetics
    git-svn-id: https://aften.svn.sourceforge.net/svnroot/aften@650 ef0d8562-5c19-0410-972e-841db63a069c

--- bindings/cs/FrameEncoder.cs
+++ bindings/cs/FrameEncoder.cs
@@ -27,8 +27,11 @@ namespace Aften
 	/// </summary>
 	public class FrameEncoder : IDisposable
 	{
+		const string _EqualAmountOfSamples = "Each channel must have an equal amount of samples.";
 		private readonly byte[] m_FrameBuffer = new byte[A52Sizes.MaximumCodedFrameSize];
+		private readonly byte[] m_StreamBuffer = new byte[sizeof( float )];
 		private readonly float[] m_Samples;
+		private readonly float[] m_StreamSamples;
 		private readonly int m_nTotalSamplesPerFrame;
 
 		private bool m_bDisposed;
@@ -88,7 +91,6 @@ namespace Aften
 			GC.SuppressFinalize( this );
 		}
 
-
 		/// <summary>
 		/// Encodes the specified interleaved samples.
 		/// </summary>
@@ -96,8 +98,24 @@ namespace Aften
 		/// <returns>MemoryStream containing the encoded frames</returns>
 		public MemoryStream Encode( float[] samples )
 		{
+			if ( samples.Length % m_Context.Channels != 0 )
+				throw new InvalidOperationException( _EqualAmountOfSamples );
+
+			return this.Encode( samples, samples.Length / m_Context.Channels );
+		}
+
+		/// <summary>
+		/// Encodes the specified interleaved samples.
+		/// </summary>
+		/// <param name="samples">The samples.</param>
+		/// <param name="samplesPerChannelCount">The samples per channel count.</param>
+		/// <returns>
+		/// MemoryStream containing the encoded frames
+		/// </returns>
+		public MemoryStream Encode( float[] samples, int samplesPerChannelCount )
+		{
 			MemoryStream stream = new MemoryStream();
-			this.Encode( samples, stream );
+			this.Encode( samples, samplesPerChannelCount, stream );
 
 			return stream;
 		}
@@ -106,29 +124,88 @@ namespace Aften
 		/// Encodes the specified interleaved samples.
 		/// </summary>
 		/// <param name="samples">The samples.</param>
-		/// <param name="stream">The stream.</param>
-		public void Encode( float[] samples, Stream stream )
+		/// <param name="frames">The frames.</param>
+		public void Encode( float[] samples, Stream frames )
 		{
 			if ( samples.Length % m_Context.Channels != 0 )
-				throw new InvalidOperationException( "Each channel must have an equal amount of samples." );
+				throw new InvalidOperationException( _EqualAmountOfSamples );
+
+			this.Encode( samples, samples.Length / m_Context.Channels, frames );
+		}
+
+		/// <summary>
+		/// Encodes the ssamplesCount of the specified interleaved samples.
+		/// </summary>
+		/// <param name="samples">The samples.</param>
+		/// <param name="samplesPerChannelCount">The samples per channel count.</param>
+		/// <param name="frames">The frames.</param>
+		public void Encode( float[] samples, int samplesPerChannelCount, Stream frames )
+		{
+			int samplesCount = samplesPerChannelCount * m_Context.Channels;
+			if ( samplesCount > samples.Length )
+				throw new InvalidOperationException( "samples contains less data then specified." );
 
 			int nOffset = m_nRemainingSamplesCount;
 			int nSamplesNeeded = m_nTotalSamplesPerFrame - nOffset;
 			int nSamplesDone = 0;
-			while ( samples.Length - nSamplesDone >= m_nTotalSamplesPerFrame ) {
+			while ( samplesCount - nSamplesDone >= m_nTotalSamplesPerFrame ) {
 				Buffer.BlockCopy( samples, nSamplesDone * sizeof( float ), m_Samples, nOffset, nSamplesNeeded * sizeof( float ) );
-				int size = aften_encode_frame( ref m_Context, m_FrameBuffer, m_Samples, A52Sizes.SamplesPerFrame );
-				if ( size < 0 )
+				int nSize = aften_encode_frame( ref m_Context, m_FrameBuffer, m_Samples, A52Sizes.SamplesPerFrame );
+				if ( nSize < 0 )
 					throw new InvalidOperationException( "Encoding error" );
 
-				stream.Write( m_FrameBuffer, 0, size );
+				frames.Write( m_FrameBuffer, 0, nSize );
 				nSamplesDone += m_nTotalSamplesPerFrame;
 				nSamplesNeeded = m_nTotalSamplesPerFrame;
 				nOffset = 0;
 			}
-			m_nRemainingSamplesCount = samples.Length - nSamplesDone;
+			m_nRemainingSamplesCount = samplesCount - nSamplesDone;
 			if ( m_nRemainingSamplesCount > 0 )
 				Buffer.BlockCopy( samples, nSamplesDone * sizeof( float ), m_Samples, 0, m_nRemainingSamplesCount * sizeof( float ) );
+		}
+
+		/// <summary>
+		/// Encodes the specified interleaved samples.
+		/// </summary>
+		/// <param name="samples">The samples.</param>
+		/// <returns>
+		/// MemoryStream containing the encoded frames
+		/// </returns>
+		public MemoryStream Encode( Stream samples )
+		{
+			MemoryStream stream = new MemoryStream();
+			this.Encode( samples, stream );
+
+			return stream;
+		}
+
+		/// <summary>
+		/// Encodes the specified interleaved samples stream and flushes the encoder.
+		/// </summary>
+		/// <param name="samples">The samples.</param>
+		/// <param name="frames">The frames.</param>
+		public void Encode( Stream samples, Stream frames )
+		{
+			if ( !samples.CanRead )
+				throw new InvalidOperationException( "Samples stream must be readable." );
+
+			int nOffset = 0;
+			int nCurrentRead;
+			while ( (nCurrentRead = samples.Read( m_StreamBuffer, 0, sizeof( float ) )) == sizeof( float ) ) {
+				m_StreamSamples[nOffset] = BitConverter.ToSingle( m_StreamBuffer, 0 );
+				++nOffset;
+				if ( nOffset == m_nTotalSamplesPerFrame ) {
+					this.Encode( m_StreamSamples, frames );
+					nOffset = 0;
+				}
+			}
+			if ( (nCurrentRead != 0) || (nOffset % m_Context.Channels != 0) )
+				throw new InvalidOperationException( _EqualAmountOfSamples );
+
+			if ( nOffset > 0 )
+				this.Encode( m_StreamSamples, nOffset / m_Context.Channels, frames );
+
+			this.Flush( frames );
 		}
 
 		/// <summary>
@@ -147,18 +224,18 @@ namespace Aften
 		/// Flushes the encoder und returns the remaining frames.
 		/// </summary>
 		/// <param name="stream">The stream.</param>
-		public void Flush( Stream stream )
+		public void Flush( Stream frames )
 		{
-			int size;
+			int nSize;
 			int nSamplesCount = m_nRemainingSamplesCount / m_Context.Channels;
 			do {
-				size = aften_encode_frame( ref m_Context, m_FrameBuffer, m_Samples, nSamplesCount );
-				if ( size < 0 )
+				nSize = aften_encode_frame( ref m_Context, m_FrameBuffer, m_Samples, nSamplesCount );
+				if ( nSize < 0 )
 					throw new InvalidOperationException( "Encoding error" );
 
-				stream.Write( m_FrameBuffer, 0, size );
+				frames.Write( m_FrameBuffer, 0, nSize );
 				nSamplesCount = 0;
-			} while ( size > 0 );
+			} while ( nSize > 0 );
 		}
 
 		/// <summary>
@@ -174,6 +251,7 @@ namespace Aften
 
 			m_nTotalSamplesPerFrame = A52Sizes.SamplesPerFrame * m_Context.Channels;
 			m_Samples = new float[m_nTotalSamplesPerFrame];
+			m_StreamSamples = new float[m_nTotalSamplesPerFrame];
 		}
 
 		/// <summary>
