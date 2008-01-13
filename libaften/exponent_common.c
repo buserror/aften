e825041796ab85833dc37887d798e3d066ce5810
jbr79 <jbr79@ef0d8562-5c19-0410-972e-841db63a069c>
2008-01-13 22:27:47 +0000

    simplify
    git-svn-id: https://aften.svn.sourceforge.net/svnroot/aften@731 ef0d8562-5c19-0410-972e-841db63a069c

--- libaften/exponent_common.c
+++ libaften/exponent_common.c
@@ -81,25 +81,19 @@ compute_exponent_strategy(A52ThreadContext *tctx)
     uint8_t *exp[A52_MAX_CHANNELS][A52_NUM_BLOCKS];
     int ch, blk, str;
 
-    if(ctx->params.expstr_fast) {
         for(ch=0; ch<ctx->n_channels; ch++) {
-            for(blk=0; blk<A52_NUM_BLOCKS; blk++) {
-                blocks[blk].exp_strategy[ch] = str_predef[2][blk];
-            }
-            frame->expstr_set[ch] = 2;
-        }
-    } else {
-        for(ch=0; ch<ctx->n_channels; ch++) {
+            str = 2;
+            if(!ctx->params.expstr_fast) {
             for(blk=0; blk<A52_NUM_BLOCKS; blk++)
                 exp[ch][blk] = blocks[blk].exp[ch];
 
             str = compute_expstr_ch(exp[ch], ncoefs[ch]);
+            }
             for(blk=0; blk<A52_NUM_BLOCKS; blk++) {
                 blocks[blk].exp_strategy[ch] = str_predef[str][blk];
             }
             frame->expstr_set[ch] = str;
         }
-    }
 
     // lfe channel
     if(ctx->lfe) {
