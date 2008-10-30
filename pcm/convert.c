d45a68b8e9615d26df95a99454d6f1e1607ce085
jbr79 <jbr79@ef0d8562-5c19-0410-972e-841db63a069c>
2008-10-30 21:42:34 +0000

    silence gcc warning by handling default case
    git-svn-id: https://aften.svn.sourceforge.net/svnroot/aften@802 ef0d8562-5c19-0410-972e-841db63a069c

--- pcm/convert.c
+++ pcm/convert.c
@@ -727,6 +727,7 @@ pcmfile_set_source_format(PcmFile *pf, enum PcmSampleFormat fmt)
         case PCM_SAMPLE_FMT_S32: SET_FMT_CONVERT_FROM(s32, pf);    break;
         case PCM_SAMPLE_FMT_FLT: SET_FMT_CONVERT_FROM(float, pf);  break;
         case PCM_SAMPLE_FMT_DBL: SET_FMT_CONVERT_FROM(double, pf); break;
+        default:                                                   break;
     }
     if (fmt == PCM_SAMPLE_FMT_FLT || fmt == PCM_SAMPLE_FMT_DBL)
         pf->sample_type = PCM_SAMPLE_TYPE_FLOAT;
